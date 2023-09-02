40eaf7fec4f5e8c11e5af1868b244a4875b5e059
Konstantin <mezentsev.k@gmail.com>
2023-09-02 11:45:51 +0800

    Задача №2 выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void yearCheck (int time) {
         if ((time % 4 == 0 && time % 100 != 0) || time % 400 == 0) {
@@ -6,11 +8,28 @@ public class Main {
             System.out.println(time + " год не является високосным");
         }
     }
+    public static void appDownload (int currentYear, int os) {
+        if (os == 0 && currentYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (os == 1 && currentYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (os == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Методы");
         System.out.println("--------------------------");
         System.out.println("Задача №1");
         int year = 2023;
         yearCheck(year);
+        //
+        System.out.println("--------------------------");
+        System.out.println("Задача №2");
+        int clientDeviceOS = 0;
+        int clientDeviceYear = LocalDate.now().getYear();
+        appDownload(year, clientDeviceOS);
         }
     }
